3ccd4d57d5e987e708129a03c7245352b2becb39
Andrew-1996 <sandrew.guna@gmail.com>
2023-03-03 14:45:23 +0530

    Props Vs States in React
    Code sections added to demonstrate properties and states in ReactJS

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import { Greet } from './components/Greet';
-
+/* import { Greet } from './components/Greet'; */
+import { Message } from './components/message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Bruce' heroName='Batman' />
+      {/* <Greet name='Bruce' heroName='Batman' />
       <Greet name='Clark' heroName='Superman' > <button> Action </button> </Greet>
-      <Greet name='Diana' heroName='Wonder Woman' > <p> This is childern props. </p> </Greet>
+      <Greet name='Diana' heroName='Wonder Woman' > <p> This is childern props. </p> </Greet> */}
+    <Message /> 
     </div>
   );
 }
