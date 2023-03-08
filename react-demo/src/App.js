a6a40eec32422714382793de4bcd541b757b74ca
Andrew-1996 <sandrew.guna@gmail.com>
2023-03-08 10:40:44 +0530

    Handling parent and child communication
    Code shows the parent - child as well as child - parent communication handling.

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { ClickHandler } from './components/ClickHandler';
-/* import { Greet } from './components/Greet'; */
-/* import { Message } from './components/message'; */
+// import { ClickHandler } from './components/ClickHandler';
+// import { Greet } from './components/Greet';
+// import { Message } from './components/message';
+import { ParentComponent } from './components/ParentComponent';
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Greet name='Clark' heroName='Superman' > <button> Action </button> </Greet>
       <Greet name='Diana' heroName='Wonder Woman' > <p> This is childern props. </p> </Greet> */}
     {/* <Message />  */}
-    <ClickHandler />
+    {/* <ClickHandler /> */}
+      <ParentComponent />
     </div>
   );
 }
